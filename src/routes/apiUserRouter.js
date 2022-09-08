251f65c79661186f4f2c69a897bc385acd61dcd2
Julia <tarasova.bmstu@gmail.com>
2022-09-08 09:53:27 +0300

    try catch

--- src/routes/apiUserRouter.js
+++ src/routes/apiUserRouter.js
@@ -6,25 +6,33 @@ const router = express.Router();
 
 router.post('/signup', async (req, res) => {
   const { login, password } = req.body;
-  const hashedPass = await bcrypt.hash(password, 10);
-  const currUser = await User.create({ login, pass: hashedPass });
-  req.session.userId = currUser.id;
-  req.session.userLogin = currUser.login;
-  //   res.sendStatus(200);
-  res.json({ name: currUser.login });
+  try {
+    const hashedPass = await bcrypt.hash(password, 10);
+    const currUser = await User.create({ login, pass: hashedPass });
+    req.session.userId = currUser.id;
+    req.session.userLogin = currUser.login;
+    //   res.sendStatus(200);
+    res.json({ name: currUser.login });
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 router.post('/signin', async (req, res) => {
   const { login, password } = req.body;
-  const currUser = await User.findOne({ where: { login } });
-  const compare = await bcrypt.compare(password, currUser.pass);
-  if (compare) {
-    req.session.userId = currUser.id;
-    req.session.userLogin = currUser.login;
-    // res.sendStatus(200);
-    res.json({ name: currUser.login });
-  } else {
-    res.sendStatus(401);
+  try {
+    const currUser = await User.findOne({ where: { login } });
+    const compare = await bcrypt.compare(password, currUser.pass);
+    if (compare) {
+      req.session.userId = currUser.id;
+      req.session.userLogin = currUser.login;
+      // res.sendStatus(200);
+      res.json({ name: currUser.login });
+    } else {
+      res.sendStatus(401);
+    }
+  } catch (error) {
+    console.log(error.message);
   }
 });
 router.get('/logout', (req, res) => {
